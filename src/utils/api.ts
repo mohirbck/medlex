5055edcdd67101f5270f0410fff03a64dfd666c5
Maher <mohirbck@gmail.com>
2025-02-12 16:35:00 +0500

    Some changes

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import { Term } from "./types";
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const API_URL = 'http://185.171.81.159:3000/api';
 
 export const termApi = {
     // Barcha terminlarni olish
